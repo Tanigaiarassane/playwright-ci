9654906d485b43532a155b53478bd6107d27dc77
Tanigai <tanigai.dj@gmail.com>
2023-11-24 20:11:58 +0530

    test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,3 +4,15 @@ test('has title', async ({ request }) => {
   const resposne = await request.get('https://reqres.in/api/users?page=2');
   expect (resposne.status()).toBe(200)
 });
+
+test('Not having title title', async ({ request }) => {
+  const resposne = await request.get('https://reqres.in/api/users?page=2');
+  expect (resposne.status()).toBe(220)
+});
+
+/*
+test('Welcome title', async ({ request }) => {
+  const resposne = await request.get('https://reqres.in/api/users?page=2');
+  expect (resposne.status()).toBe(200)
+});
+*/
